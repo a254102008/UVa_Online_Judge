57013b5d1960fd776f299a1d40c9a3c7dbc11eec
a254102008 <a254102008@yahoo.com.tw>
2016-01-27 21:54:21 +0800

    Create UVa12478 Hardest Problem Ever (Easy).cpp

---
+++ UVa12478 Hardest Problem Ever (Easy).cpp
@@ -0,0 +1,12 @@
+/*******************************************************/
+/* UVa 12478 Hardest Problem Ever (Easy)               */
+/* Author: Ching-Yang-Hsieh                            */
+/* Version: 2016/01/27                                 */
+/*******************************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "KABIR" << endl; // prints !!!Hello World!!!
+	return 0;
+}
